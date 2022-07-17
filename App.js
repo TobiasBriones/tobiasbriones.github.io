9bb1103f8be2261a64c9666b64b045debe606e86
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-07-16 23:25:52 -0600

    Add temporal page

---
+++ App.js
@@ -0,0 +1,49 @@
+const URL = `https://api.github.com/users/tobiasbriones/repos`;
+
+export default {
+  data() {
+    return { projects: [] };
+  },
+  async mounted() {
+    const res = await fetch(URL);
+    this.projects = await res.json();
+  },
+  methods: {
+    home(project) {
+      return `https://dev.mathsoftware.engineer/${ project.name }`;
+    }
+  },
+  template: `
+    <div class="m-5">
+      <h1 class="mx-4 my-4">
+        Projects ({{ projects.length }}) | DEV | Math Software Engineer
+      </h1>
+      <div class="profile mx-4">
+        <img src="https://github.com/tobiasbriones.png" alt="Tobias Briones">
+        <div class="my-2"><strong>Tobias Briones</strong></div>
+        <a href="https://github.com/tobiasbriones">GitHub</a>
+        -
+        <a href="https://twitter.com/tobiasbriones">Twitter</a>
+        -
+        <a href="https://linkedin.com/u/tobiasbriones">LinkedIn</a>
+      </div>
+      
+      <div>
+        Notice: This is a temporal page design while I'm working on other stuff...
+      </div>
+      
+      <div class="grid grid-cols-2 gap-4 mx-5 my-4 content-center cursor-pointer project">
+          {{ project }}
+        <a :href="home(project)"
+          v-for="project in projects">
+          <div class="px-4 py-4 name">
+            {{ project.name }}
+          </div>
+          <div class="px-4 align-middle description">
+            {{ project.description }}
+          </div>
+        </a>
+      </div>
+    </div>
+  `
+};
